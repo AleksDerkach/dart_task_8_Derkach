d27355e369e1bd05eb6ea67346c47cc661696b49
Sasha Derkach <dosprg@gmail.com>
2025-06-30 11:37:01 +0300

    solution task_8_2

--- task_8_1.dart
+++ task_8_1.dart
@@ -1,5 +1,6 @@
 void main(List<String> args) {
    task_8_1();
+   task_8_2();
 }
 
 void task_8_1() {
@@ -10,4 +11,24 @@ void task_8_1() {
   print(list);
   list.removeAt(4);
   print(list);
+}
+
+void task_8_2() {
+ 
+  var setTypeKnown = {'int', 'double', 'String', 'bool', 
+                    'List', 'Set', 'Map', 'Null'};
+    
+  var setTypeCore = setTypeKnown.map((t) => (t)).toSet();
+  setTypeCore.addAll({'Records', 'Function', 'Runes', 'Symbols'});
+  
+  print('Same types');
+  final setSameType = setTypeKnown.intersection(setTypeCore);
+  print(setSameType);
+
+  print('different types');
+  for (final typeIn in setTypeCore) {
+    if (!setTypeKnown.contains(typeIn)) {
+      print(typeIn);
+    } 
+  }
 }
