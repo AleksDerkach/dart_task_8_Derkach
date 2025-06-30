8aedfbb26c8beeae67ebccb1da3852942aa4024a
Sasha Derkach <dosprg@gmail.com>
2025-06-30 09:41:04 +0300

    solution task_8_1

--- task_8_1.dart
+++ task_8_1.dart
@@ -1,3 +1,13 @@
 void main(List<String> args) {
-   
+   task_8_1();
+}
+
+void task_8_1() {
+ 
+  var list = ['Pascal', 'C++', 'Assembler', 'Delphi', '1C'];
+  print(list);
+  list.add("Flutter");
+  print(list);
+  list.removeAt(4);
+  print(list);
 }
